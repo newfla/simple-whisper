50e7d6fb22ea047cd203c247d4a502a1d3f017e6
newfla <newfla@hotmail.it>
2024-07-04 18:23:09 +0200

    ci: fix ubuntu runner

--- simple-whisper/src/models.rs
+++ simple-whisper/src/models.rs
@@ -6,7 +6,6 @@ use tokio::sync::mpsc::{unbounded_channel, UnboundedSender};
 
 use crate::{Error, Event};
 
-
 /// OpenAI supported models
 #[derive(Default, Clone, Debug, EnumIter, EnumString, Display)]
 #[strum(serialize_all = "snake_case")]
